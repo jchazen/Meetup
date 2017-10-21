e1a244aade883b410aa56e4f8ea059577fde2333
jchaz <jchazen@ucsd.edu>
2017-10-21 11:53:18 -0700

    Add project files.

---
+++ Meetup/Controllers/MeetupController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+using Twilio.TwiML;
+using Twilio.AspNet.Mvc;
+
+namespace Meetup.Controllers
+{
+    public class MeetupController : TwilioController
+    {
+        // GET: Meetup
+        public ActionResult SendSms()
+        {
+            
+        }
+    }
+}
